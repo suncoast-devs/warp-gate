7d71cd23697e3929167ff631e473a1c9e5aaf271
Gavin Stark <gavin@gstark.com>
2020-10-01 13:31:25 -0400

    Adds a full usable repository for calculator
    This makes it easier to hack on the template but we only include the bits we need

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 
 export function Calculator() {
@@ -8,24 +7,24 @@ export function Calculator() {
         <div className="display">0</div>
         <div className="buttons">
           <button className="button fn">AC</button>
-          <button className="button fn">&#177;</button>
-          <button className="button fn">&#37;</button>
-          <button className="button op">&#247;</button>
+          <button className="button fn">&plusmn;</button>
+          <button className="button fn">%</button>
+          <button className="button op">&divide;</button>
           <button className="button">7</button>
           <button className="button">8</button>
           <button className="button">9</button>
-          <button className="button op">&#215;</button>
+          <button className="button op">&times;</button>
           <button className="button">4</button>
           <button className="button">5</button>
           <button className="button">6</button>
-          <button className="button op">&#8722;</button>
+          <button className="button op">&minus;</button>
           <button className="button">1</button>
           <button className="button">2</button>
           <button className="button">3</button>
-          <button className="button op">&#43;</button>
+          <button className="button op">+</button>
           <button className="button x2">0</button>
           <button className="button">.</button>
-          <button className="button op">&#61;</button>
+          <button className="button op">=</button>
         </div>
       </div>
     </main>
