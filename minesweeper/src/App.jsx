d6c3e1ff6f672592556e98b4d1aba357c470d3d7
Gavin Stark <gavin@gstark.com>
2021-09-13 10:45:15 -0400

    Adds minesweeper initial project files

---
+++ minesweeper/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function App() {
+  return (
+    <main>
+      <h1>Mine Sweeper</h1>
+      <h2>
+        <button>New Easy Game</button>
+        <button>New Intermediate Game</button>
+        <button>New Difficult Game</button>
+      </h2>
+      <h3>Mines: 100</h3>
+      <h3>Game #: 200</h3>
+      <h3></h3>
+
+      <ul>
+        <li> </li>
+      </ul>
+    </main>
+  )
+}
